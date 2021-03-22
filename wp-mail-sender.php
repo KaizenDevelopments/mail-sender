bbb223c0e2fe6ec7326ccbab0e9d98ed4184222c
Jean-Philippe Bidegain <jp@bidega.in>
2021-03-22 10:57:08 +0100

    Improve plugin top comment

--- wp-mail-sender.php
+++ wp-mail-sender.php
@@ -1,12 +1,25 @@
 <?php
 /*
-Plugin Name: WP Mail Sender
-Version: 1.0
-Plugin URI: https://wordpress.org/plugins/wp-mail-sender/
-Description: Simple plugin to fix the mail sender enveloppe and use the From: address
-Author: Kaizen Developments
-Author URI: https://www.kaizen-developments.com
-*/
+ * WP Mail Sender
+ *
+ * Plugin Name: WP Mail Sender
+ * Version:     1.0
+ * Plugin URI:  https://wordpress.org/plugins/wp-mail-sender/
+ * Description: Simple plugin to fix the mail sender enveloppe and use the From: address
+ * Author:      Kaizen Developments
+ * Author URI:  https://www.kaizen-developments.com
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: wp-mail-sender
+ * Domain Path: /languages
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
